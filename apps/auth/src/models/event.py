aa22bef00b7dba4e5b609aa55d8270b9e96f35f4
Alexandr Rutkowski <kitanoyoru@protonmail.com>
2023-10-06 14:32:37 +0300

    dev(auth): start db work

---
+++ apps/auth/src/models/event.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from datetime import date
+from sqlalchemy import Boolean, DateTime, Integer, String, func
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class LogModel(Base):
+    __tablename__ = "event"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+
+    is_error: Mapped[bool] = mapped_column(Boolean)
+    message: Mapped[str] = mapped_column(String, nullable=False) 
+
+    created_at: Mapped[date] = mapped_column(DateTime, default=func.now(), nullable=False)
